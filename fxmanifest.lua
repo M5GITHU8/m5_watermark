16bc9c77dbe708b4bc49b66b44101d613a76e8e6
M5GITHU8 <lamardavis2911@gmail.com>
2025-11-04 19:12:27 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+author 'M5'
+description 'discord.gg/mi5'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/img/*.png',
+    'html/index.html',
+}
+
+dependency '/onesync'
